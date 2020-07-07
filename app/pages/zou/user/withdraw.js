bff9dbcf3cd0d6126817f4ed962bc25acd4f4f4d
faaat <zjf580@163.com>
2020-07-07 15:36:23 +0800

    fix url

--- app/pages/zou/user/withdraw.js
+++ app/pages/zou/user/withdraw.js
@@ -201,7 +201,7 @@ export default class app extends Component {
         key: 'operate',
         render: (text, record) => (
           <span>
-            <a onClick={() => browserHistory.push(`/gold/${record.user_id}`)}>金币明细</a>
+            <a onClick={() => browserHistory.push(`/zou-gold/${record.user_id}`)}>金币明细</a>
             <br />
             {record.withdraw_status === 'pending' ?
               (<span>
