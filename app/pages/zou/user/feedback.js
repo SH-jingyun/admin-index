f8c0f76a34144dc6c7bd6cd939ce7cc57271dfe5
faaat <zjf580@163.com>
2020-07-07 15:51:03 +0800

    fix image url

--- app/pages/zou/user/feedback.js
+++ app/pages/zou/user/feedback.js
@@ -6,7 +6,7 @@ import TableList from '@tableList';
 import {
   zouFeedback,
 } from '@apis/manage';
-import { mockURL } from '@config';
+import { zouURL } from '@config';
 
 const { Content } = Layout;
 
@@ -93,19 +93,19 @@ export default class app extends Component {
         title: '反馈图片1',
         dataIndex: 'image_1',
         key: 'image_1',
-        render: text => (text ? <img className="auto_img" src={`${mockURL}/${text}`} /> : ''),
+        render: text => (text ? <img className="auto_img" src={`${zouURL}/${text}`} /> : ''),
       },
       {
         title: '反馈图片2',
         dataIndex: 'image_2',
         key: 'image_2',
-        render: text => (text ? <img className="auto_img" src={`${mockURL}/${text}`} /> : ''),
+        render: text => (text ? <img className="auto_img" src={`${zouURL}/${text}`} /> : ''),
       },
       {
         title: '反馈图片3',
         dataIndex: 'image_3',
         key: 'image_3',
-        render: text => (text ? <img className="auto_img" src={`${mockURL}/${text}`} /> : ''),
+        render: text => (text ? <img className="auto_img" src={`${zouURL}/${text}`} /> : ''),
       },
       {
         title: '发生时间',
